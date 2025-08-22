55439785b371c4135fbaa081e8fbb81b1889edad
Eric Dallo <ericdallo06@hotmail.com>
2025-08-22 16:08:07 -0300

    Add --eca-* vars

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from "./App";
 import './index.css';
+import { editorName } from "./util";
+
+const editor = editorName() || 'vscode';
+document.documentElement.dataset.editor = editor;
 
 (async () => {
     ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
